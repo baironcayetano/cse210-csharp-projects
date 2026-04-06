b38e3897e26d39ec87fd7213a452464347734d4c
baironcayetano <baironjuega01@gmail.com>
2026-04-06 01:00:00 -0600

    Week05 - Learning Activity  - Completed

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -3,7 +3,15 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+    {   
+        MathAssignment mathAssignment = new MathAssignment("Roberto Rodriguez","Fractions","7.3","8-9");
+        Console.WriteLine("Math Assignment:");
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+        
+        WritingAssignment writingAssignment = new WritingAssignment("Mary Waters","European History","The causes of World War II");
+        Console.WriteLine("WritingAssignment:");
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInformation());
     }
 }
