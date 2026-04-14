3ab1fa9556c88dd6e121b73a43c053222c76be48
baironcayetano <baironjuega01@gmail.com>
2026-04-14 01:59:15 -0600

    Week06- Learning Activity - Polymorphism

---
+++ week06/Shapes/Circle.cs
@@ -0,0 +1,14 @@
+class Circle : Shape
+{
+    private double _radius;
+
+    public Circle(double radius)
+    {
+        _radius = radius;
+    }
+
+    public override double GetArea()
+    {
+        return Math.PI * _radius * _radius;
+    }
+}
--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,24 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        Rectangle rectangle = new Rectangle(7,5);
+        rectangle.SetColor("Blue");
+
+        Circle circle = new Circle(4);
+        circle.SetColor("Green");
+        
+        Square square = new Square(5);
+        square.SetColor("Red");
+
+        List<Shape> shapes = new List<Shape>{rectangle, circle, square};
+        foreach(Shape shape in shapes)
+        {
+            Console.WriteLine($"The area of the shape is {shape.GetArea()} and its color is {shape.GetColor()}");
+        }
+   
     }
 }
